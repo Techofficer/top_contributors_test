a9273363288a4c1d099f18941f3e1ba288a407d1
Artem Efremov <efremov.artserg@gmail.com>
2019-04-23 21:18:49 +0300

    Initial Commit

---
+++ Top Githubers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Githubers
+//
+//  Created by Артем Ефремов on 23/04/2019.
+//  Copyright © 2019 Artem Efremov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Top GithubersTests/Top_GithubersTests.swift
@@ -0,0 +1,34 @@
+//
+//  Top_GithubersTests.swift
+//  Top GithubersTests
+//
+//  Created by Артем Ефремов on 23/04/2019.
+//  Copyright © 2019 Artem Efremov. All rights reserved.
+//
+
+import XCTest
+@testable import Top_Githubers
+
+class Top_GithubersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
